f2fa1917e16167f99736b5ae50a411cfcc1276b6
FilipGaida99 <42692696+BoomboxPL1@users.noreply.github.com>
2021-06-06 12:41:28 +0200

    Working method
    Not good work, but work

--- Forecaster.py
+++ Forecaster.py
@@ -1,96 +1,69 @@
-import operator
-import os
-import datetime
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
-import seaborn as sns
-from pylab import rcParams
 import matplotlib.pyplot as plt
-from matplotlib import rc
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import Bidirectional, Dropout, Activation, Dense, LSTM
-from tensorflow.python.keras.layers import CuDNNLSTM
-from tensorflow.keras.models import Sequential
-from random import seed
-from random import random
-
-def to_sequences(data, seq_len):
-    d = []
-
-    for index in range(len(data) - seq_len):
-        d.append(data[index: index + seq_len])
-
-    return np.array(d)
+import pandas as pd
+import tensorflow as tf
+from PredictionWindow import WindowGenerator, compile_and_fit
 
 
-def preprocess(data_raw, seq_len, days):
-    data = to_sequences(data_raw, seq_len)
-
-    num_train = data.shape[0] - days
-
-    x_train = data[:num_train, :-1, :]
-    y_train = data[:num_train, -1, :]
-
-    return x_train, y_train
-
-
-def forecast(data_path, days):
-    BATCH_SIZE = 64
-    SEQ_LEN = 100
-    DROPOUT = 0.2
-    WINDOW_SIZE = SEQ_LEN - 1
+def forecast(data_path, days, plot_test=False):
+    out_steps = int(days)
 
     df = pd.read_csv(data_path)
     df['Date'] = pd.to_datetime(df.Date, infer_datetime_format=True)
-    base = df['Date'].max() + datetime.timedelta(days=1)
-    seed(2137)
-    dfa = pd.DataFrame({'Date': [base + datetime.timedelta(days=x) for x in range(days)]})
-    #df = df.append(dfa, ignore_index=True)
     df.sort_values(by=['Date'], ascending=True, inplace=True, ignore_index=True)
 
-    #scaler = MinMaxScaler()
+    date_time = pd.to_datetime(df.pop('Date'), format='%d.%m.%Y %H:%M:%S')
+    column_indices = {name: i for i, name in enumerate(df.columns)}
 
-    #close_price = df.Close.values.reshape(-1, 1)
-
-    #scaled_close = scaler.fit_transform(close_price)
-
-    #scaled_close = scaled_close[~np.isnan(scaled_close)]
-    #scaled_close = scaled_close.reshape(-1, 1)
-
-    #x_train, y_train, x_predict = \
-    #    preprocess(scaled_close, SEQ_LEN, days)
+    n = len(df)
+    train_df = df[0:int(n * 0.7)]
+    val_df = df[int(n * 0.7):int(n * 0.9)]
+    test_df = df[int(n * 0.9):]
 
     num_features = df.shape[1]
-    OUT_STEPS = 20
-    model = tf.keras.Sequential([
+
+    train_mean = train_df.mean()
+    train_std = train_df.std()
+
+    train_df = (train_df - train_mean) / train_std
+    val_df = (val_df - train_mean) / train_std
+    test_df = (test_df - train_mean) / train_std
+
+    multi_window = WindowGenerator(input_width=out_steps,
+                                   label_width=out_steps,
+                                   shift=out_steps,
+                                   train_df=train_df,
+                                   val_df=val_df,
+                                   test_df=test_df)
+
+    multi_lstm_model = tf.keras.Sequential([
         # Shape [batch, time, features] => [batch, lstm_units]
         # Adding more `lstm_units` just overfits more quickly.
         tf.keras.layers.LSTM(32, return_sequences=False),
         # Shape => [batch, out_steps*features]
-        tf.keras.layers.Dense(OUT_STEPS * num_features,
+        tf.keras.layers.Dense(out_steps * num_features,
                               kernel_initializer=tf.initializers.zeros()),
         # Shape => [batch, out_steps, features]
-        tf.keras.layers.Reshape([OUT_STEPS, num_features])
+        tf.keras.layers.Reshape([out_steps, num_features])
     ])
 
-    model.add(Activation('linear'))
+    history = compile_and_fit(multi_lstm_model, multi_window)
+    predicts = multi_lstm_model.predict(multi_window.test)
+    predicts_data = predicts
+    sums = predicts_data[0]
+    for x in predicts_data[1:]:
+        sums += x
+    predicts_data = sums / predicts.shape[0]
+    predicts_data = predicts_data * train_std[3] + train_mean[3]
+    if plot_test:
+        plt.plot(predicts_data[:, 3], label="Predicted price", color='red')
+        test_data = test_df['Close'] * train_std[3] + train_mean[3]
+        test_data.reset_index(drop=True, inplace=True)
+        test_data = test_data[:out_steps]
+        plt.plot(test_data, label="Actual price", color='green')
+        plt.xlabel('Days')
+        plt.ylabel('Price(USD)')
+        plt.title('Dogecoin Price Prediction')
+        plt.legend(loc='best')
+        plt.show()
 
-    model.compile(
-        loss='mean_squared_error',
-        optimizer='adam'
-    )
-
-    history = model.fit(
-        x_train,
-        y_train,
-        epochs=50,
-        batch_size=BATCH_SIZE,
-        shuffle=True,
-        validation_split=0.1
-    )
-
-    y_hat = model.predict(x_predict)
-    #y_hat_inverse = scaler.inverse_transform(y_hat)
-    return y_hat
+    return predicts_data
--- main.py
+++ main.py
@@ -1,28 +1,3 @@
-import operator
-import os
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import pandas as pd
-import seaborn as sns
-from pylab import rcParams
-import matplotlib.pyplot as plt
-from matplotlib import rc
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.layers import Bidirectional, Dropout, Activation, Dense, LSTM
-from tensorflow.python.keras.layers import CuDNNLSTM
-from tensorflow.keras.models import Sequential
 from Forecaster import forecast
 
-
-y_hat_inverse = forecast('Dogecoin-original.csv', 120)
-
-#plt.plot(y_test_inverse, label="Actual Price", color='green')
-plt.plot(y_hat_inverse, label="Predicted Price", color='red')
-
-plt.title('Dogecoin price prediction')
-plt.xlabel('Time [days]')
-plt.ylabel('Price')
-plt.legend(loc='best')
-
-plt.show()
+forecast('Dogecoin-original.csv', 40, True)
