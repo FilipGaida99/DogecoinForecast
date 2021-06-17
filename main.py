8dee728a48a30929737da33685cab8de0d471370
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2021-06-17 17:06:27 +0200

    optional main arg to set days to predicate

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+from sys import argv
+
 from forecast.Forecaster import forecast
 
 if __name__ == '__main__':
-    forecast('datasets/Dogecoin-original.csv', 40, True)
+    days = 40
+    if len(argv) > 1:
+        days = argv[1]
+    forecast('datasets/Dogecoin-original.csv', days, True)
