3f34ca3041cb698a06e0b24e30ffe2bd3a4b1d1c
FilipGaida99 <42692696+BoomboxPL1@users.noreply.github.com>
2021-06-19 18:00:57 +0200

    better prediction

--- forecast/Forecaster.py
+++ forecast/Forecaster.py
@@ -16,14 +16,22 @@ def forecast(data_path, days, plot_test=False):
 
     n = len(df)
 
-    last_date = (df[:df.shape[0] - out_steps])['Date'].iloc[-1]
+    if plot_test:
+        last_date = (df[:df.shape[0] - out_steps])['Date'].iloc[-1]
+    else:
+        last_date = df['Date'].iloc[-1]
     last_date = last_date + datetime.timedelta(days=1)
     date_time = pd.to_datetime(df.pop('Date'), format='%d.%m.%Y %H:%M:%S')
     column_indices = {name: i for i, name in enumerate(df.columns)}
 
+    if not plot_test:
+        empty_data = []
+        for i in range(0,out_steps):
+            empty_data.insert(0, {'Open': 0.0,'High':0.0,'Low':0.0,'Close':0.0,'Volume':0.0,'Market Cap':0.0})
+        df = df.append(pd.DataFrame(empty_data), ignore_index=True)
     train_df = df[:df.shape[0] - out_steps]
     val_df = train_df[int(train_df.shape[0] * 0.95):]
-    test_df = df
+    test_df = df[df.shape[0] - 2 * out_steps:]
 
     num_features = df.shape[1]
 
@@ -34,7 +42,7 @@ def forecast(data_path, days, plot_test=False):
     val_df = (val_df - train_mean) / train_std
     test_df = (test_df - train_mean) / train_std
 
-    multi_window = WindowGenerator(input_width=out_steps,
+    multi_window = WindowGenerator(input_width=1,
                                    label_width=out_steps,
                                    shift=out_steps,
                                    train_df=train_df,
@@ -57,17 +65,17 @@ def forecast(data_path, days, plot_test=False):
     predicts_data = predicts[-1]
     for x in range(0, predicts_data.shape[0]):
         predicts_data[x] = predicts_data[x] * train_std + train_mean
-    if plot_test:
-        test_data = test_df[out_steps:]
-        test_data = test_data * train_std + train_mean
-        test_data.reset_index(drop=True, inplace=True)
-        date_range = pd.date_range(start=last_date, periods=out_steps)
-        candle_plot(predicts_data, test_data[test_data.shape[0] - out_steps:], date_range)
+    predicts_data = predicts_data[::-1]
+    test_data = test_df
+    test_data = test_data * train_std + train_mean
+    test_data.reset_index(drop=True, inplace=True)
+    date_range = pd.date_range(start=last_date, periods=out_steps)
+    candle_plot(predicts_data, test_data[test_data.shape[0] - out_steps:], date_range, plot_test)
 
     return predicts_data
 
 
-def candle_plot(predicts_data, test_data, date_time):
+def candle_plot(predicts_data, test_data, date_time, plot_test):
     if type(predicts_data) is not pd.DataFrame:
         if isinstance(predicts_data, np.ndarray):
             predicts_data = np.delete(predicts_data, [4, 5], 1)
@@ -81,9 +89,11 @@ def candle_plot(predicts_data, test_data, date_time):
     if type(test_data) is not pd.DataFrame:
         raise TypeError("Expect test_data as pandas.DataFrame.")
 
-    test_data['Datetime'] = pd.to_datetime(date_time)
-    test_data = test_data.set_index('Datetime')
+    if plot_test:
+        test_data['Datetime'] = pd.to_datetime(date_time)
+        test_data = test_data.set_index('Datetime')
 
     title = 'Dogecoin Price Prediction'
     mpf.plot(predicts_data, type='candle', style='charles', title=title, ylabel='Price [USD]')
-    mpf.plot(test_data, type='candle', style='charles', title=title+'_test', ylabel='Price [USD]')
+    if plot_test:
+        mpf.plot(test_data, type='candle', style='charles', title=title+'_test', ylabel='Price [USD]')
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sys import argv
 from forecast.Forecaster import forecast
 
 if __name__ == '__main__':
-    days = 10
+    days = 20
     if len(argv) > 1:
         days = argv[1]
-    forecast('datasets/Dogecoin-original.csv', days, True)
+    forecast('datasets/Dogecoin-original.csv', days, False)
